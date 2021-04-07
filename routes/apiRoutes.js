252940c6f53e2e96e20132719afb04e8146410af
mushtaq <mushtaq.safie@students.mq.edu.au>
2021-04-07 19:43:45 +1000

    create new workout request

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -13,10 +13,24 @@ module.exports = function(app) {
     });
   });
 
+  // create new workout request
   app.post("/api/workouts", function(req, res) {
-    db.Workout.create(req.body).then(dbNewWorkout => {
-      console.log(dbNewWorkout);
-      res.josn(dbNewWorkout);
+    db.Workout.create(req).then(newWorkout => {
+        console.log(newWorkout);
+        res.json(newWorkout);
+      })
+      .catch(result => {
+        console.log(result);
+      });
+  });
+
+  // update existing workout, adding new exercise
+  app.put("/api/workouts/:id", function(req, res) {
+    db.Workout.update(
+      { _id: req.params.id },
+      { exercises: req.body }
+    ).then(function(dbWorkouts) {
+      res.json(dbWorkouts);
     })
     .catch(({ message }) => {
       console.log(message);
@@ -24,22 +38,4 @@ module.exports = function(app) {
   
   });
 
-  app.put("/api/workouts", function(req, res) {
-    db.Workout.create(req.body).then(dbNewWorkout => {
-      console.log(dbNewWorkout);
-      res.josn(dbNewWorkout);
-    })
-    .catch(({ message }) => {
-      console.log(message);
-    });
-  
-  });
-  // app.put("/api/images/:id", function(req, res) {
-  //   db.Image.updateOne(
-  //     { _id: req.params.id },
-  //     { rating: req.body.rating }
-  //   ).then(function(dbImage) {
-  //     res.json(dbImage);
-  //   });
-  // });
 };
